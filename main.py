c793a88a236c798066ebf58705e9cdc2e5c8b092
Rizwan Hasan <rizwan.hasan486@gmail.com>
2020-10-24 19:02:58 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from PIL import Image
 import os
 import platform
 
-
 def make_pdf_all(dir_path):
-
     if platform.system() == "Windows":
         join = str("/")
         dir_files = [dir_path + join + f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
@@ -15,7 +13,6 @@ def make_pdf_all(dir_path):
         dir_files = [dir_path + join + f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
         return dir_files
 
-
 def make_pdf_only_selected(selected_files, file_name, pdf_location):
     images_list = []
     for f in selected_files:
@@ -25,7 +22,6 @@ def make_pdf_only_selected(selected_files, file_name, pdf_location):
             pass
     os.chdir(pdf_location)
     images_list[0].save(file_name, save_all=True, append_images=images_list[1:])
-
 
 if __name__ == '__main__':
     print("This Is The Main Backend Module!")
